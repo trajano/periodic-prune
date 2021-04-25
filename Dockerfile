01398a5470aa748bdc4344cef3afe8084fe5f601
Archimedes Trajano <archimedes.trajano@devhaus.com>
2021-04-25 12:15:43 -0400

    Added healthcheck

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ ENV CONTAINER_UNTIL=30m \
     IMAGE_UNTIL=2h \
     IMAGE_ALL_UNTIL=48h
 CMD [ "watch", "-t",  "-n", "3600" , "/prune" ]
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD [ "curl", "-s", "--no-buffer", "--unix-socket", "/var/run/docker.sock", http://localhost/ping ]
--- prune.sh
+++ prune.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/containers/prune?filters=%7B%22until%22%3A%5B%22${CONTAINER_UNTIL}%22%5D%7D
 curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_UNTIL}%22%5D%2C%22dangling%22%3A%5B%221%22%5D%7D
 curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_ALL_UNTIL}%22%5D%2C%22dangling%22%3A%5B%220%22%5D%7D
