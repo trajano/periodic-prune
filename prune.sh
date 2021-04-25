51fa68a9a271a1fa52911a0a5f7a6275fae4f3f1
Archimedes Trajano <archimedes.trajano@devhaus.com>
2021-04-25 09:25:19 -0400

    Typo should've used watch instead of sleep

--- prune.sh
+++ prune.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-curl -s -q -o /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/containers/prune?filters=%7B%22until%22%3A%5B%2230m%22%5D%7D
-sleep 60m
+watch -t -n 3600 curl -s -q -o /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/containers/prune?filters=%7B%22until%22%3A%5B%2230m%22%5D%7D
