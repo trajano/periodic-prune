4d9076d7c94d31f6bc93562da8e6a8fc8e8a07e1
Archimedes Trajano <developer@trajano.net>
2022-08-16 07:58:17 -0400

    feat: exclusions and remove output to logs

--- README.md
+++ README.md
@@ -8,10 +8,38 @@ This container runs a shell script that prunes stopped containers.  This helps h
 
 To run this.
 
-    docker run -d --restart=always -v //var/run/docker.sock:/var/run/docker.sock --name periodic-prune trajano/periodic-prune
+    docker run -d --restart=always -v //var/run/docker.sock:/var/run/docker.sock:ro --name periodic-prune trajano/periodic-prune
+
+## Exclusions
+
+The image exclusions prevent specific images from being pruned to avoid downloading again.  These are passed in as a white space separated list into the environment.  Quoted values are not supported.
+
+    docker run -d --restart=always -v //var/run/docker.sock:/var/run/docker.sock:ro --name periodic-prune \
+      --env EXCLUSIONS="
+        alpine
+        node:16.16.0
+        gradle:7.4-jdk17
+        openjdk:17-jdk
+        gcr.io/distroless/java17-debian11
+      " \
+      trajano/periodic-prune
+
+This is implememented by creating containers, but not running them; then executing the prune operation; then removing the temporary containers.  This does not pull if the images are not present.
+
 
 ## Developer notes
 
 Testing locally
 
     docker run -v //var/run/docker.sock:/var/run/docker.sock:ro  `docker build -q  . ` //prune
+
+    docker run -v //var/run/docker.sock:/var/run/docker.sock:ro  \
+      --env EXCLUSIONS="
+        alpine
+        node:16.16.0
+        gradle:7.4-jdk17
+        openjdk:17-jdk
+        gcr.io/distroless/java17-debian11
+      " \
+      `docker build -q  . ` \
+      //prune
--- prune.sh
+++ prune.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -e
-curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/containers/prune?filters=%7B%22until%22%3A%5B%22${CONTAINER_UNTIL}%22%5D%7D
-curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_UNTIL}%22%5D%2C%22dangling%22%3A%5B%221%22%5D%7D
-curl -s --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_ALL_UNTIL}%22%5D%2C%22dangling%22%3A%5B%220%22%5D%7D
+curl -s --output /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/containers/prune?filters=%7B%22until%22%3A%5B%22${CONTAINER_UNTIL}%22%5D%7D
+
+name_prefix=R${RANDOM}
+for image in $EXCLUSIONS
+do
+  name=${name_prefix}_$(echo $image | sha1sum | awk '{ print $1 }')
+  curl -s --output /dev/null --output /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock -H "Content-Type: application/json" --data "{ \"Image\": \"${image}\" }" http://localhost/containers/create?name=${name}
+done
+curl -s --output /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_UNTIL}%22%5D%2C%22dangling%22%3A%5B%221%22%5D%7D
+curl -s --output /dev/null --no-buffer -XPOST --unix-socket /var/run/docker.sock http://localhost/images/prune?filters=%7B%22until%22%3A%5B%22${IMAGE_ALL_UNTIL}%22%5D%2C%22dangling%22%3A%5B%220%22%5D%7D
+for image in $EXCLUSIONS
+do
+  name=${name_prefix}_$(echo $image | sha1sum | awk '{ print $1 }')
+  curl -s --output /dev/null --no-buffer -XDELETE --unix-socket /var/run/docker.sock http://localhost/containers/${name}
+done
